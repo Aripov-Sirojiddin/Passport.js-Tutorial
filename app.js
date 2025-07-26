5374503832d095509c15548a9028e7a4cba5c7e6
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-26 11:17:17 +0500

    Init commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const assetsPath = path.join(__dirname, "public");
 const PORT = process.env.PORT || 3000;
